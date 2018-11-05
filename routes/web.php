be0cd35e7ed8a7dde83bca1c1f6c8fb36015625e
vagrant <vagrant@homestead>
2018-11-05 08:24:05 +0000

    finish layout and roututes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
-
+// 主页
 Route::get('/', 'StaticPagesController@home');
-Route::get('/help', 'StaticPagesController@help');
-Route::get('/about', 'StaticPagesController@about');
+// 帮助页面
+Route::get('/help', 'StaticPagesController@help')->name('help');
+// 关于页面
+Route::get('/about', 'StaticPagesController@about')->name('about');
+
+
+// 注册页面
+Route::get('/signup', 'UsersController@create')->name('signup');
