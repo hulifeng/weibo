77b7ebe751ee41c4fee527a58ce8d449fc9df59c
vagrant <vagrant@homestead>
2018-11-05 10:05:34 +0000

    finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $confirm = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($confirm, $request->has('remember'))) {
+            // 登录成功操作
+            session()->flash('success', '欢迎您回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            // 登录失败操作
+            session()->flash('danger', '抱歉，您的账号或者密码有误，请重新登录！');
+            return redirect()->back();
+        }
+    }
+
+    public function destory()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,7 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('/signup', 'UsersController@create')->name('signup');
 // 用户路由资源
 Route::resource('users', 'UsersController');
+
+Route::get('login', 'SessionsController@create')->name('login');
+Route::post('login', 'SessionsController@store')->name('login');
+Route::delete('logout', 'SessionsController@destory')->name('logout');
